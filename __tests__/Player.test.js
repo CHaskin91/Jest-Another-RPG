673ce3e92e57919ad3595d9ecde4fe5c224f5696
Chubbs91 <chaskin1991@outlook.com>
2022-01-26 13:39:42 -0600

    work on player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -12,6 +12,12 @@ test('creates a player object', () => {
     expect(player.agility).toEqual(expect.any(Number));
   });
 
+  test("gets player's health value", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+  });
+
   test("gets player's stats as an object", () => {
     const player = new Player('Dave');
   
@@ -19,6 +25,16 @@ test('creates a player object', () => {
     expect(player.getStats()).toHaveProperty('health');
     expect(player.getStats()).toHaveProperty('strength');
     expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+  
+    expect(player.isAlive()).toBeTruthy();
+  
+    player.health = 0;
+  
+    expect(player.isAlive()).toBeFalsy();
   });
 
   test('gets inventory from player or returns false', () => {
@@ -29,4 +45,17 @@ test('creates a player object', () => {
     player.inventory = [];
   
     expect(player.getInventory()).toEqual(false);
+  });
+
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
   });
